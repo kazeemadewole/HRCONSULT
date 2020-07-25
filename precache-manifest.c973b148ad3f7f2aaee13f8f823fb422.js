a891179a69858c893f51e8870e091fa2caebc067
whocares0 <adebayoadewole858@gmail.com>
2020-07-25 07:31:32 +0100

    Updates

---
+++ precache-manifest.c973b148ad3f7f2aaee13f8f823fb422.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c84c343a77ed207f698a400612b5bb5",
+    "url": "/HRCONSULT/index.html"
+  },
+  {
+    "revision": "d8e50cdd022337a398b3",
+    "url": "/HRCONSULT/static/css/2.e40c9195.chunk.css"
+  },
+  {
+    "revision": "eff9104a7a72877c43a0",
+    "url": "/HRCONSULT/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8e50cdd022337a398b3",
+    "url": "/HRCONSULT/static/js/2.587b0db5.chunk.js"
+  },
+  {
+    "revision": "e0546182649f50a2eb923c2a881ed896",
+    "url": "/HRCONSULT/static/js/2.587b0db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff9104a7a72877c43a0",
+    "url": "/HRCONSULT/static/js/main.517822ca.chunk.js"
+  },
+  {
+    "revision": "32f29bb0fa90f65c4f02",
+    "url": "/HRCONSULT/static/js/runtime-main.11a3c221.js"
+  }
+]);
